a69e1786ae408f1c31b019256555b99576cf1bc0
simondlevy <simon.d.levy@gmail.com>
2023-10-11 18:19:51 -0400

    Compiling Linux example

--- linux/src/vl53l1x_linux.cpp
+++ linux/src/vl53l1x_linux.cpp
@@ -18,4 +18,48 @@ VL53L1_Error VL53L1_WaitUs(VL53L1_Dev_t *pdev, int32_t usec)
     return VL53L1_ERROR_NONE;
 }
 
+
+VL53L1_Error VL53L1_WrWord(VL53L1_Dev_t *pdev, uint16_t index, uint16_t data)
+{
+    (void)pdev;
+    (void)index;
+    (void)data;
+
+    return VL53L1_ERROR_NONE;
+}
+
+
+VL53L1_Error VL53L1_poll_for_boot_completion(VL53L1_Dev_t *pdev,
+        uint32_t timeout_ms)
+{
+    (void)pdev;
+    (void)timeout_ms;
+
+    return VL53L1_ERROR_NONE;
+}
+
+VL53L1_Error VL53L1_is_new_data_ready(VL53L1_Dev_t *pdev, uint8_t *pready)
+{
+    (void)pdev;
+    (void)pready;
+
+    return VL53L1_ERROR_NONE;
+}
+
+VL53L1_Error VL53L1_poll_for_range_completion(VL53L1_Dev_t *pdev, 
+        uint32_t timeout_ms)
+{
+    (void)pdev;
+    (void)timeout_ms;
+
+    return VL53L1_ERROR_NONE;
+}
+
+VL53L1_Error VL53L1_wait_for_boot_completion(VL53L1_Dev_t *pdev)
+{
+    (void)pdev;
+
+    return VL53L1_ERROR_NONE;
+}
+
 }
