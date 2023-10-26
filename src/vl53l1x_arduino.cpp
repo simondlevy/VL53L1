9ead9ae1a076df605dbf35d473d322aaaa64a5ee
simondlevy <simon.d.levy@gmail.com>
2023-10-26 19:15:12 -0400

    Cleanup

--- src/vl53l1x_arduino.cpp
+++ src/vl53l1x_arduino.cpp
@@ -4,15 +4,15 @@
 extern "C" {
 #include "vendor/vl53l1_api.h"
 
-VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t rgstr, 
+VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *dev, uint16_t rgstr, 
         uint8_t * pdata, uint32_t   count)
 {
-    TwoWire * twoWire = (TwoWire *)pdev->I2Cx;
+    TwoWire * twoWire = (TwoWire *)dev->I2Cx;
 
     //Loop until the port is transmitted correctly
     while (true) {
 
-        twoWire->beginTransmission(pdev->devAddr);
+        twoWire->beginTransmission(dev->devAddr);
         uint8_t buffer[2];
         buffer[0]=(uint8_t) (rgstr>>8);
         buffer[1]=(uint8_t) (rgstr&0xFF);
@@ -33,7 +33,7 @@ VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t rgstr,
         }
     }
 
-    twoWire->requestFrom((int)pdev->devAddr, (int) count);
+    twoWire->requestFrom((int)dev->devAddr, (int) count);
 
     int i=0;
     while (twoWire->available()) {
@@ -45,12 +45,12 @@ VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t rgstr,
     return VL53L1_ERROR_NONE;
 }
 
-VL53L1_Error VL53L1_WriteMulti(VL53L1_Dev_t *pdev, uint16_t rgstr, 
+VL53L1_Error VL53L1_WriteMulti(VL53L1_Dev_t *dev, uint16_t rgstr, 
         uint8_t * data, uint32_t count)
 {
-    TwoWire * twoWire = (TwoWire *)pdev->I2Cx;
+    TwoWire * twoWire = (TwoWire *)dev->I2Cx;
 
-    twoWire->beginTransmission(pdev->devAddr);
+    twoWire->beginTransmission(dev->devAddr);
 
     static uint8_t buffer[256];
     buffer[0]=(uint8_t) (rgstr>>8);
