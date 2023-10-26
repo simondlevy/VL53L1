f01e1b3401def347cab530156fedb5e42f9afb9f
simondlevy <simon.d.levy@gmail.com>
2023-10-26 14:49:33 -0400

    index => rgstr

--- src/vl53l1x_arduino.cpp
+++ src/vl53l1x_arduino.cpp
@@ -4,7 +4,7 @@
 extern "C" {
 #include "vendor/vl53l1_api.h"
 
-VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t index, 
+VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t rgstr, 
         uint8_t * pdata, uint32_t   count)
 {
     TwoWire * twoWire = (TwoWire *)pdev->I2Cx;
@@ -16,8 +16,8 @@ VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t index,
     {
         twoWire->beginTransmission(pdev->devAddr);
         uint8_t buffer[2];
-        buffer[0]=(uint8_t) (index>>8);
-        buffer[1]=(uint8_t) (index&0xFF);
+        buffer[0]=(uint8_t) (rgstr>>8);
+        buffer[1]=(uint8_t) (rgstr&0xFF);
         twoWire->write(buffer, 2);
         status = twoWire->endTransmission(false);
         //Fix for some STM32 boards
@@ -45,7 +45,7 @@ VL53L1_Error VL53L1_ReadMulti(VL53L1_Dev_t *pdev, uint16_t index,
     return VL53L1_ERROR_NONE;
 }
 
-VL53L1_Error VL53L1_WriteMulti(VL53L1_Dev_t *pdev, uint16_t index, 
+VL53L1_Error VL53L1_WriteMulti(VL53L1_Dev_t *pdev, uint16_t rgstr, 
         uint8_t * data, uint32_t count)
 {
     TwoWire * twoWire = (TwoWire *)pdev->I2Cx;
@@ -53,8 +53,8 @@ VL53L1_Error VL53L1_WriteMulti(VL53L1_Dev_t *pdev, uint16_t index,
     twoWire->beginTransmission(pdev->devAddr);
 
     static uint8_t buffer[256];
-    buffer[0]=(uint8_t) (index>>8);
-    buffer[1]=(uint8_t) (index&0xFF);
+    buffer[0]=(uint8_t) (rgstr>>8);
+    buffer[1]=(uint8_t) (rgstr&0xFF);
     memcpy(&buffer[2], data, count);
 
     twoWire->write(buffer, count+2);
